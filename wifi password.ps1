8c5559b718fa1d1588c7d553895783cd7f5abfe8
icebear <79372739+icebearhacks@users.noreply.github.com>
2021-12-03 14:02:11 -0500

    Add files via upload

---
+++ wifi password.ps1
@@ -0,0 +1,5 @@
+﻿netsh wlan show profiles
+print "Which network do you want the password for?"
+#have the user create a variable here with the network name
+#
+$wifi = netsh wlan show profile name="NETWORK" key=clear
